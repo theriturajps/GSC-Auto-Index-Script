4da2a51817035cd19d05de72e27c42f9a200d971
RituRajPS <theriturajps@gmail.com>
2024-04-07 13:21:34 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,58 @@
+# Google Search Console Auto-Indexing Script (GSC-Auto-Indexing)
+
+This script will automatically submit all your website URLs to Google Search Console for indexing (or re-indexing).
+
+There are heaps of paid SEO tools available to automatically submit your sites to Google for indexing. They're great, but you can also use this script to do the same thing for free.
+
+This script uses a Google Cloud Service Account to connect to your Google Search Console instance, pull in your sitemap, and submit each page automatically for indexing.
+
+Plus, it's easy to get started.
+
+## Setup
+
+Setting up this script is easy. You just need to create a (free) Google Cloud service account, authenticate this script using the `.json` service account keys, then invite the service account to your Google Search Console account.
+
+0. Clone this script and install packages
+
+   Download this script directly, or clone it. Make sure you've got `ruby` installed locally, then run `bundle` to install all required packages —
+
+   ```sh
+   git clone git@github.com:theriturajps/GSC-Auto-Index-Script.git
+   cd GSC-Auto-Index-Script
+   bundle
+   ```
+1. Create a service account in Google Cloud
+
+   - Visit https://console.cloud.google.com/iam-admin/serviceaccounts and create a new service account (click the big `+ Create Service Account` button).
+   - Click "Actions" > "Manage keys" > "Add key" > "Create new key" > "JSON". A `.json` key file will automatically download — move it into this project directory
+2. Authenticate the script
+
+   - Edit `script.rb` and change the line with `service_account_file = "_.json" to be the name of your `.json` key file.
+   - For example,
+
+   ```ruby
+   service_account_file = "main_service_account.json"
+   ```
+3. Invite your service account into your Google Search Console
+
+   - Open Google Search Console and, in the sidebar, click "Settings" > "Users and Permissions" > "Add User" and enter the email of your service account
+     - ie: "`example@project-400005.iam.gserviceaccount.com`"
+     - Set the permissions for the account to "Owner"
+4. Run the script!
+
+   - Run `ruby script.rb` to start the script. It will automatically pull in the sites available in your Google Search Console, and let you choose between them.
+   - Once you choose a site, it will parse the sitemap (from the URL within Google Search Console), and submit each page for indexing.
+
+## Optional Configuration and Flags
+
+### `INDEX_FROM_CSV`
+
+If this flag is set `true`, the script will look for a CSV file called `urls.csv`. This file should contain a comma-separated list of URLs to index, like this —
+
+```csv
+https://www.riturajps.in, https://riturajps.in/p/about-us.html
+```
+
+This can be helpful for sites with a massive number of pages, so you can index a subset rather than all of them.
+
+By default, this flag is set `false`.
---
+++ script.rb
@@ -0,0 +1,120 @@
+require "googleauth" # google client login
+require "google/apis/searchconsole_v1"
+require "open-uri" # sitemap parsing
+require "xmlsimple"
+require "tty-prompt" # terminal prompts
+
+require "csv" # optional csv parsing
+
+## Optional Flags
+#
+# If this is set to true, the script read from a CSV file called "urls.csv"
+# This file should contain URLs that you want to index for your site.
+# If this flag is true, we will SKIP parsing your sitemap and just use the urls.csv values directly.
+INDEX_FROM_CSV = false
+
+# Preface: functions we use later on #
+def get_all_site_urls(service)
+  sites = service.list_sites
+  sites.site_entry.map { |site| site.site_url }
+end
+
+# Get the sitemap URL from within Google Search Console, for a selected site
+# site_url: string: "https://riturajps.in" | "sc-domain:riturajps.in"
+def get_sitemap_url(service, site_url)
+  sitemaps = service.list_sitemaps(site_url)
+  sitemaps.sitemap[0].path
+end
+
+# Get and parse the sitemap of a URL into individual URLs
+def get_all_pages_from_sitemap(url)
+  xml_data = URI.open(url).read
+  data = XmlSimple.xml_in(xml_data)
+  links = data["url"].map { |url| url["loc"][0] }
+end
+
+def get_all_pages_from_csv(path)
+  urls = []
+  CSV.foreach(path) do |row|
+    # Since all URLs are in one line, just grab the first line
+    urls = row
+  end
+  urls
+end
+
+# Submit a single page url for indexing to Google Search Console
+def submit_index_request(service, site_url, page_url)
+  request = Google::Apis::SearchconsoleV1::InspectUrlIndexRequest.new
+  request.inspection_url = page_url
+  request.site_url = site_url
+  response = service.inspect_url_index(request)
+
+  return response
+end
+
+### Step 0: Get setup in Google Cloud and Google Search Console
+# Create a new service account —
+# - Visit https://console.cloud.google.com/iam-admin/serviceaccounts and create a new service account
+# - Click "Actions" > "Manage keys" > "Add key" > "Create new key" > "JSON", and move the ".json" file into the same directory as this script
+# - Edit "service_account_file" to match the name of your key — 
+#
+service_account_file = "main_service_account.json"
+#
+# - Invite your service account into your Google Search Console
+# - Search Console > "Settings" > "Users and Permissions" > "Add User" and enter the email of your service account
+#   - ie: "example@project-400005.iam.gserviceaccount.com"
+#   - Set permissions to "Owner"
+
+### Step 1: Create and auth client with Google APIs
+scope = "https://www.googleapis.com/auth/webmasters"
+authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
+  json_key_io: File.open(service_account_file),
+  scope: scope,
+)
+
+authorizer.fetch_access_token!
+service = Google::Apis::SearchconsoleV1::SearchConsoleService.new
+service.authorization = authorizer
+
+### Step 2: Run app
+prompt = TTY::Prompt.new
+
+# pick a site from all the ones we have access to
+all_site_urls = get_all_site_urls(service)
+selected_site_url = prompt.select("Select a site — ", all_site_urls)
+
+# determine which pages to index
+pages_to_index = []
+
+if INDEX_FROM_CSV
+  # If this flag is enabled, pull pages from CSV
+  csv_pages = get_all_pages_from_csv("urls.csv")
+  pp "Found #{csv_pages.count} pages in urls.csv — "
+  pp csv_pages
+
+  pages_to_index = csv_pages
+else
+  # Otherwise, fetch sitemap and pull pages from it
+  # get the sitemap URL from GSC, then parse it an extract all pages
+  sitemap_url = get_sitemap_url(service, selected_site_url)
+  sitemap_pages = get_all_pages_from_sitemap(sitemap_url)
+  pp "Found #{sitemap_pages.count} pages in sitemap for #{selected_site_url} — "
+  pp sitemap_pages
+
+  pages_to_index = sitemap_pages
+end
+
+# do the indexing
+should_index = prompt.yes?("Should we submit all these pages for (re)indexing?")
+if should_index
+  pages_to_index.each do |page|
+    response = submit_index_request(service, selected_site_url, page)
+
+    current_status = response.inspection_result.index_status_result.coverage_state
+    last_crawled = response.inspection_result.index_status_result.last_crawl_time
+    pp "(#{page}) | Current status: #{current_status} | Last crawled: #{last_crawled.nil? ? "never" : Time.new(last_crawled).localtime}"
+  end
+  pp "indexed #{pages_to_index.count} pages for #{selected_site_url}"
+end
+
+pp "Done."
